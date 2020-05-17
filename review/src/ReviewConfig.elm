3fdbf0a2e500813b40694e06914eb04a8d04180e
Phill Sparks <me@phills.me.uk>
2020-05-17 16:25:15 +0100

    Apply NoModuleOnExposedNames rule to project

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -1,21 +1,11 @@
 module ReviewConfig exposing (config)
 
-{-| Do not rename the ReviewConfig module or the config function, because
-`elm-review` will look for these.
-
-To add packages that contain rules, add them to this review project using
-
-    `elm install author/packagename`
-
-when inside the directory containing this file.
-
--}
-
 import NoDebug.Log
 import NoDebug.TodoOrToString
 import NoExposingEverything
 import NoImportingEverything
 import NoInconsistentAliases
+import NoModuleOnExposedNames
 import NoMissingTypeAnnotation
 import NoUnused.CustomTypeConstructors
 import NoUnused.Dependencies
@@ -37,6 +27,7 @@ config =
         ]
         |> NoInconsistentAliases.rule
     , NoMissingTypeAnnotation.rule
+    , NoModuleOnExposedNames.rule
     , NoUnused.CustomTypeConstructors.rule []
     , NoUnused.Dependencies.rule
     , NoUnused.Exports.rule
